384cc24721162066935cac1d1b3b297833b792f5
CarnunMP <carnun@hotmail.co.uk>
2019-09-13 09:20:36 +0100

    basic styling done

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,12 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledCharacterBox = styled.div`
+    border: 2px solid black;
+    border-radius: 5px;
+    margin: 1rem 0 1rem 0;
+    padding: 1rem;
+    background: grey;
+`;
 
 function Character(props) {
     const {name, gender, homeworld, species} = props;
 
     return (
-        <div>
-            {name}
-        </div>
+        <StyledCharacterBox>
+            <h3>Name: {name}</h3>
+            <h3>Gender: {gender}</h3>
+            <h3>Homeworld: {homeworld}</h3>
+            <h3>Species: {species}</h3>
+        </StyledCharacterBox>
     )
 }
 
